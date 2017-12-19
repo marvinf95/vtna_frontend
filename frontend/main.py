fc5532a42b69fbe6a5846aa9b67f1b9c99505ec0
AlexBaier <abaier@uni-koblenz.de>
2017-12-19 19:37:37 +0100

    Added most UI functions to UIDataUploadManager. Missing: open_column_config

--- frontend/main.py
+++ frontend/main.py
@@ -12,11 +12,10 @@ import vtna.layout
 import vtna.utility
 
 
-NETWORK_UPLOAD_PLACEHOLDER = 'Enter URL -> Click Upload'
-LOCAL_UPLOAD_PLACEHOLDER = 'Click on Upload -> Select file'
-
-
 class UIDataUploadManager(object):
+    NETWORK_UPLOAD_PLACEHOLDER = 'Enter URL -> Click Upload'  # type: str
+    LOCAL_UPLOAD_PLACEHOLDER = 'Click on Upload -> Select file'  # type: str
+
     def __init__(self,
                  # Graph upload widgets
                  local_graph_file_upload: fileupload.FileUploadWidget,
@@ -45,6 +44,9 @@ class UIDataUploadManager(object):
     def get_edge_list(self) -> typ.List[vtna.data_import.TemporalEdge]:
         return self.__edge_list
 
+    def get_metadata(self) -> vtna.data_import.MetadataTable:
+        return self.__metadata
+
     def build_on_toggle_upload_type(self) -> typ.Callable:
         # TODO: What is the type of change? Dictionary?
         def on_toogle_upload_type(change):
@@ -58,9 +60,9 @@ class UIDataUploadManager(object):
                 self.__network_metadata_upload_button.layout.display = 'inline'
                 # Enable text input for URLs
                 self.__graph_data_text.disabled = False
-                self.__graph_data_text.placeholder = NETWORK_UPLOAD_PLACEHOLDER
+                self.__graph_data_text.placeholder = UIDataUploadManager.NETWORK_UPLOAD_PLACEHOLDER
                 self.__metadata_data_text.disabled = False
-                self.__metadata_data_text.placeholder = NETWORK_UPLOAD_PLACEHOLDER
+                self.__metadata_data_text.placeholder = UIDataUploadManager.NETWORK_UPLOAD_PLACEHOLDER
             # Switch to local upload option
             else:
                 # Show local upload widgets
@@ -71,35 +73,107 @@ class UIDataUploadManager(object):
                 self.__network_metadata_upload_button.layout.display = 'none'
                 # Disable text input for local upload
                 self.__graph_data_text.disabled = True
-                self.__graph_data_text.placeholder = LOCAL_UPLOAD_PLACEHOLDER
+                self.__graph_data_text.placeholder = UIDataUploadManager.LOCAL_UPLOAD_PLACEHOLDER
                 self.__metadata_data_text.disabled = True
-                self.__metadata_data_text.placeholder = LOCAL_UPLOAD_PLACEHOLDER
+                self.__metadata_data_text.placeholder = UIDataUploadManager.LOCAL_UPLOAD_PLACEHOLDER
         return on_toogle_upload_type
 
-    def handle_local_upload_graph_data(change):
-        # What does the w stand for?
-        w = change['owner']
-        try:
-            with open(w.filename, 'wb') as f:
-                f.write(w.data)
-                w_file_graph_data.value = 'Uploaded `{}` ({:.2f} kB)'.format(w.filename, len(w.data) / 2 ** 10)
+    def build_handle_local_upload_graph_data(self) -> typ.Callable:
+        def handle_local_upload_graph_data(change):
+            # TODO: What does the w stand for?
+            w = change['owner']
+            try:
+                # Upload and store file to notebook directory
+                # TODO: put it into a tmp folder
+                with open(w.filename, 'wb') as f:
+                    f.write(w.data)
+                    self.__graph_data_text.value = w.filename
+                # Import graph as edge list via vtna
+                self.__edge_list = vtna.data_import.read_edge_table(w.filename)
+                # Display summary of loaded file to __graph_data_output
+                self.__display_graph_upload_summary(w.filename)
+            # TODO: Exception catching is not exhaustive yet
+            except FileNotFoundError:
+                error_msg = f'File {w.filename} does not exist'
+                self.__display_graph_upload_error(error_msg)
+            except ValueError:
+                error_msg = f'Columns 1-3 in {w.filename} cannot be parsed to integers'
+                self.__display_graph_upload_error(error_msg)
+        return handle_local_upload_graph_data
 
-            edge_list = vtna.data_import.read_edge_table(w.filename)
+    def build_handle_network_upload_graph_data(self) -> typ.Callable:
+        def handle_network_upload_graph_data(change):
+            try:
+                url = self.__graph_data_text.value
+                self.__edge_list = vtna.data_import.read_edge_table(url)
+                self.__display_graph_upload_summary(url)
+            # TODO: Exception catching is not exhaustive yet
+            except urllib.error.HTTPError:
+                error_msg = f'Could not access URL {url}'
+                self.__display_graph_upload_error(error_msg)
+            except ValueError:
+                error_msg = f'Columns 1-3 in {url} cannot be parsed to integers'
+                self.__display_graph_upload_error(error_msg)
+        return handle_network_upload_graph_data
 
-            with w_out_graph_data:
-                ipydisplay.clear_output()
-                print_edge_stats(w.filename, edge_list)
-        # TODO: Exception catching is not exhaustive yet
-        except FileNotFoundError:
-            w_file_graph_data.value = f'File {w.filename} does not exist'
-            with w_out_graph_data:
-                ipydisplay.clear_output()
-                print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-        except ValueError:
-            w_file_graph_data.value = f'Columns 1-3 in {w.filename} cannot be parsed to integers'
-            with w_out_graph_data:
-                ipydisplay.clear_output()
-                print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
+    def build_handle_local_upload_metadata(self) -> typ.Callable:
+        def handle_local_upload_metadata(change):
+            w = change['owner']
+            try:
+                with open(w.filename, 'wb') as f:
+                    f.write(w.data)
+                    self.__graph_data_text.value = w.filename
+                # Load metadata
+                metadata_table = vtna.data_import.MetadataTable(w.filename)
+                self.__display_metadata_upload_summary(w.filename)
+                # Display UI for configuring metadata
+                open_column_config(metadata_table)
+            # TODO: Exception catching is not exhaustive yet
+            except FileNotFoundError:
+                error_msg = f'File {w.filename} does not exist'
+                self.__display_metadata_upload_error(error_msg)
+            except ValueError:
+                error_msg = f'Column 1 in {w.filename} cannot be parsed to integer'
+                self.__display_metadata_upload_error(error_msg)
+        return handle_local_upload_metadata
+
+    def build_handle_network_upload_metadata(self) -> typ.Callable:
+        def handle_network_upload_metadata(change):
+            try:
+                url = self.__metadata_data_text.value
+                metadata_table = vtna.data_import.MetadataTable(url)
+                self.__display_metadata_upload_summary(url)
+                open_column_config(metadata_table)
+            # TODO: Exception catching is not exhaustive yet
+            except urllib.error.HTTPError:
+                error_msg = f'Could not access URL {url}'
+                self.__display_metadata_upload_error(error_msg)
+            except ValueError:
+                error_msg = f'Column 1 in {url} cannot be parsed to integer'
+                self.__display_metadata_upload_error(error_msg)
+        return handle_network_upload_metadata
+
+    def __display_graph_upload_error(self, msg: str):
+        self.__graph_data_text.value = msg
+        with self.__graph_data_output:
+            ipydisplay.clear_output()
+            print(f'\x1b[31m{msg}\x1b[0m')
+
+    def __display_graph_upload_summary(self, filename: str):
+        with self.__graph_data_output:
+            ipydisplay.clear_output()
+            print_edge_stats(filename, self.__edge_list)
+
+    def __display_metadata_upload_error(self, msg):
+        self.__metadata_data_text.value = msg
+        with self.__metadata_data_output:
+            ipydisplay.clear_output()
+            print(f'\x1b[31m{msg}\x1b[0m')
+
+    def __display_metadata_upload_summary(self, filename: str):
+        with self.__metadata_data_output:
+            ipydisplay.clear_output()
+            print_metadata_stats(filename, self.__metadata)
 
 
 w_metadata_settings_left = None  # type: widgets.VBox
@@ -107,99 +181,19 @@ w_metadata_settings_left = None  # type: widgets.VBox
 box_layout_ = None  # type: widgets.Layout
 
 
-def print_edge_stats(filename, edges):
-    print('Preview {} :'.format(filename))
+def print_edge_stats(filename: str, edges: typ.List[vtna.data_import.TemporalEdge]):
+    print(f'Preview {filename}:')
     print('Total Edges:', len(edges))
     print('Update Delta:', vtna.data_import.infer_update_delta(edges))
     print('Time Interval:', vtna.data_import.get_time_interval_of_edges(edges))
 
 
-def print_metadata_stats(metadata):
-    pass
-
-
-# why the underscore?
-# Upload of temporal graph data via local upload
-
-
-
-# Upload of temporal graph data via network
-def handle_network_upload_graph_data(change):
-    global edge_list
-    global w_out_graph_data
-    global w_file_graph_data
-    try:
-        url = w_file_graph_data.value
-        edge_list = vtna.data_import.read_edge_table(url)
-        with w_out_graph_data:
-            ipydisplay.clear_output()
-            print_edge_stats(url, edge_list)
-    # TODO: Exception catching is not exhaustive yet
-    except urllib.error.HTTPError:
-        w_file_graph_data.value = f'Could not access URL {url}'
-        with w_out_graph_data:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-    except ValueError:
-        w_file_graph_data.value = f'Columns 1-3 in {url} cannot be parsed to integers'
-        with w_out_graph_data:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-
-
-# Upload of metadata via local upload
-def handle_local_upload_metadata(change):
-    global metadata_table
-    global w_file_metadata
-    global w_out_metadata
-    try:
-        w = change['owner']
-        with open(w.filename, 'wb') as f:
-            f.write(w.data)
-            w_file_metadata.value = 'Uploaded `{}` ({:.2f} kB)'.format(w.filename, len(w.data) / 2 ** 10)
-        # Load metadata
-        metadata_table = vtna.data_import.MetadataTable(w.filename)
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print_metadata_stats(metadata_table)
-        # Load new UI for configuring metadata
-        open_column_config(metadata_table)
-    # TODO: Exception catching is not exhaustive yet
-    except FileNotFoundError:
-        w_file_metadata.value = f'File {w.filename} does not exist'
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-    except ValueError:
-        w_file_metadata.value = f'Column 1 in {w.filename} cannot be parsed to integer'
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-
-
-# Upload of metadata via network
-def handle_network_upload_metadata(change):
-    global metadata_table
-    global w_file_metadata
-    global w_out_metadata
-    try:
-        url = w_file_metadata.value
-        metadata_table = vtna.data_import.MetadataTable(url)
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print_metadata_stats(metadata_table)
-        open_column_config(metadata_table)
-    # TODO: Exception catching is not exhaustive yet
-    except urllib.error.HTTPError:
-        w_file_metadata.value = f'Could not access URL {url}'
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
-    except ValueError:
-        w_file_metadata.value = f'Column 1 in {url} cannot be parsed to integer'
-        with w_out_metadata:
-            ipydisplay.clear_output()
-            print(f'\x1b[31m{w_file_graph_data.value}\x1b[0m')
+def print_metadata_stats(filename: str, metadata: vtna.data_import.MetadataTable):
+    print(f'Preview {filename}:')
+    for idx, name in enumerate(metadata.get_attribute_names()):
+        print(f'Column {name}:')
+        for category in metadata.get_categories(name):
+            print(f'- {category}')
 
 
 def open_column_config(metadata: vtna.data_import.MetadataTable):
@@ -211,11 +205,11 @@ def open_column_config(metadata: vtna.data_import.MetadataTable):
     # Load some default settings
     w_metadata_settings_left.children = []
 
-    for idx, name in enumerate(metadata.get_attribute_names()):
+    for name in enumerate(metadata.get_attribute_names()):
         w_col_name = widgets.Text(
             value='{}'.format(name),
-            placeholder='Name',
-            description=f'Column {idx}:',
+            placeholder='New Column Name',
+            description=f'Column {name}:',
             disabled=False
         )
         w_metadata_settings_left.children.append(widgets.VBox([w_col_name], layout=box_layout_))
