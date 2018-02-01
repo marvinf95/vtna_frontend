ea2cc66ff3343e1ae5a81eb3c2b32dd1cd4ce4ff
Philipp Töws <ptoews@uni-koblenz.de>
2018-02-02 00:58:35 +0100

    Try to synchronize javascript code

--- frontend/js/export.js
+++ frontend/js/export.js
@@ -1,8 +1,10 @@
+var busy = false
 // This is just a callback construct, so python output and errors
 // (stdout + stderr) on executing a kernel command can be
 // viewed in a browser console.
 function handle_output(data){
     console.log(data.content);
+    busy = false
 }
 var callbacks = {
         iopub : {
@@ -21,14 +23,26 @@ var removePlot = function(id) {
     }
 }
 
+var waitWhileBusy = function() {
+    if (busy) {
+        setTimeout(waitWhileBusy, 100)
+    }
+}
+
 var extractPlotlyImage = function () {
     // Returns a Promise
     // We choose the second element with that class, because the first one
     // is our real plot
+    //while (busy) {}
+    console.log("Extracting image...");
     Plotly.toImage(document.getElementsByClassName("plotly-graph-div")[1])
         .then(function(imgData) {
+            console.log("Extracting image done!")
             // Remove data URL prefix and call main's callback method
             var command = "display_manager.write_export_frame(b'" + imgData.replace("data:image/png;base64,", "") + "')";
-            IPython.notebook.kernel.execute(command, callbacks);
+            busy = true
+            sync.fiber(function(){
+                IPython.notebook.kernel.execute(command, callbacks);
+            });
     });
 }
