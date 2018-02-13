335985aea852cc75c129eb560d23d9901cdb0d37
Philipp Töws <ptoews@uni-koblenz.de>
2018-02-13 23:06:09 +0100

    Revert splitting in favor of better approach

--- frontend/main.py
+++ frontend/main.py
@@ -498,14 +498,16 @@ class UIDataUploadManager(object):
     def __display_measure_selection(self, container_box: widgets.Box):
         # Reset internal widget dict
         self.__measure_selection_checkboxes = {}
-        html = "<h2>Available Measures:</h2>"
+        header = widgets.HTML("<h2>Available Measures:</h2>")
+        widget_list = [header]
         for measure_name in NodeMeasuresManager.node_measure_types:
             self.__measure_selection_checkboxes[measure_name] = widgets.Checkbox(
                 value=False,
                 description=NodeMeasuresManager.node_measure_types[measure_name].get_name(),
                 disabled=False
             )
-        container_box.children = [widgets.HTML(html)]
+            widget_list.append(self.__measure_selection_checkboxes[measure_name])
+        container_box.children = widget_list
 
 
 def print_edge_stats(edges: typ.List[vtna.data_import.TemporalEdge]):
@@ -1632,26 +1634,18 @@ def build_predicate(raw_predicate: typ.Dict, attribute_info: typ.Dict) \
 
 
 class NodeMeasuresManager(object):
-    local_node_measure_types = {
+    # A dictionary is used for easier returning of specific measures
+    node_measure_types = {
         centrality.get_name(): centrality for centrality in
         [
             vtna.node_measure.LocalDegreeCentrality,
-            vtna.node_measure.LocalBetweennessCentrality,
-            vtna.node_measure.LocalClosenessCentrality,
-        ]
-    }
-    global_node_measure_types = {
-        centrality.get_name(): centrality for centrality in
-        [
             vtna.node_measure.GlobalDegreeCentrality,
+            vtna.node_measure.LocalBetweennessCentrality,
             vtna.node_measure.GlobalBetweennessCentrality,
+            vtna.node_measure.LocalClosenessCentrality,
             vtna.node_measure.GlobalClosenessCentrality
         ]
     }
-    # A dictionary is used for easier returning of specific measures
-    # This dict is a union of the both previous ones
-    node_measure_types = dict(local_node_measure_types)
-    node_measure_types.update(global_node_measure_types)
 
     def __init__(self, temporal_graph: vtna.graph.TemporalGraph, requested_node_measures: typ.List[str]):
         """
